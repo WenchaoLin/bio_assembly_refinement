cfbc585cae4d1128c15e6833178cd63c9d0afa27
Martin Hunt <mh12@sanger.ac.uk>
2016-04-18 16:03:34 +0100

    Implement pymummer.__version__

--- bio_assembly_refinement/__init__.py
+++ bio_assembly_refinement/__init__.py
@@ -1,3 +1,12 @@
+from pkg_resources import get_distribution
+
+try:
+    __version__ = get_distribution('bio_assembly_refinement').version
+except:
+    __version__ = 'local'
+
+
+
 __all__ = [ 'contig_cleanup',
 			'contig_overlap_trimmer',
 			'contig_break_finder',
